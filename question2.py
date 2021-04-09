d70cffc5e4bf31bf00760104414e4b6b1d6dca6e
dunyaoguz <dunya.oguz@mail.mcgill.ca>
2021-04-09 00:57:27 -0400

    create recursive function that finds nfa states

--- question2.py
+++ question2.py
@@ -1,5 +1,6 @@
 from PySimpleAutomata import automata_IO
 
+
 def read_nfa():
     """ Reads the transition table given by the user for an nfa """
 
@@ -26,13 +27,61 @@ def read_nfa():
     initial = [state.strip('->') for state in states if '->' in state]
     final = [state.strip('*') for state in states if '*' in state]
     states = [state.strip('->').strip('*') for state in states]
+    rows = [[r.strip('->').strip('*') for r in row] for row in rows]
 
     return rows, alphabet, states, initial, final
 
-def convert_nfa(nfa):
+
+def find_new_states(transitions, new_states):
+    """ Recursively finds the new states generated during the nfa-to-dfa conversion"""
+    # base case
+    if not new_states:
+        return transitions
+
+    new = {}
+    for k, v in transitions.items():
+        if len(v) > 1:
+            for letter in rows[0][1:]:
+                r = []
+                for _ in v:
+                    if (_, letter) in transitions.keys():
+                        r.extend(list(transitions[(_, letter)]))
+                new[(str(v), letter)] = set(r)
+
+    current_states = [k[0] for k in transitions.keys()]
+    new_states = [v for v in new.values() if len(v) > 1
+                  and str(v) not in current_states]
+    return find_new_states(transitions | new, new_states)
+
+
+def convert_nfa(rows):
     """ Converts the nfa in the transition table to a dfa """
 
-    return None
+    transitions = {}
+    for row in rows[1:]:
+        for i, cell in enumerate(row[1:]):
+            key = (row[0], rows[0][i+1])
+            for c in cell.split('|'):
+                if c != '':
+                    transitions.setdefault(key, set()).add(c)
+
+    new_states = [v for v in transitions.values() if len(v) > 1]
+    return find_new_states(transitions, new_states)
+
+
+def create_output(alphabet, states, initial, final, transitions):
+    """ Creates a transition table and diagram for the new dfa """
+
+    # Q: How to determine the initial state of the new dfa?
+    # Q: How to determine the final state of the new dfa?
+    # Q: Is it ok to not include those additional rows in the table?
+
+    nfa = {'alphabet': alphabet,
+           'states': states,
+           'accepting_states': final,
+           'transitions': transitions}
+
 
 if __name__ == '__main__':
     rows, alphabet, states, initial, final = read_nfa()
+    transitions = convert_nfa(rows)
